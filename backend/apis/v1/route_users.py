d001b43a81530c6d3667deb0f22776943bd72741
VladMerk <merkvladimir@gmail.com>
2024-03-30 20:40:02 +0300

    fix: route_users

--- backend/apis/v1/route_users.py
+++ backend/apis/v1/route_users.py
@@ -1,5 +1,5 @@
 from typing import Sequence
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, Request
 from sqlalchemy import Result, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -12,16 +12,21 @@ from db.repository.user import create_new_user
 router = APIRouter(prefix="/api/v1/users", tags=["Users"])
 
 
-@router.get("/")
-async def get_users(session: AsyncSession = Depends(session_dependency)) -> list[ShowUser]:
+@router.get("/", response_model=list[ShowUser])
+async def get_users(
+    session: AsyncSession = Depends(session_dependency),
+) -> list[ShowUser]:
     result: Result = await session.execute(select(User))
     users: Sequence[User] = result.scalars().all()
     return [ShowUser.model_validate(user) for user in users]
 
 
-@router.post("/")
+@router.post("/", response_model=ShowUser)
 async def create_user(
-    user_in: UserCreate, session: AsyncSession = Depends(session_dependency)
+    email: str = Form(...),
+    password: str = Form(...),
+    session: AsyncSession = Depends(session_dependency),
 ) -> ShowUser:
+    user_in = UserCreate(email=email, password=password)
     user: User = await create_new_user(user_in, session)
     return ShowUser.model_validate(user)
